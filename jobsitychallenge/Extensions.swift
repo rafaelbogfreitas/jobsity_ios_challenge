50a9074f612872e5503d5c704f929e5f90d00663
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-16 13:59:13 -0300

    Add new extensions to treat html strings

--- jobsitychallenge/Extensions.swift
+++ jobsitychallenge/Extensions.swift
@@ -18,3 +18,24 @@ extension UINavigationController {
         coordinator.animate(alongsideTransition: nil) { _ in completion?() }
     }
 }
+
+extension Data {
+    var html2AttributedString: NSAttributedString? {
+        do {
+            return try NSAttributedString(data: self, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            print("error:", error)
+            return  nil
+        }
+    }
+    var html2String: String { html2AttributedString?.string ?? "" }
+}
+
+extension StringProtocol {
+    var html2AttributedString: NSAttributedString? {
+        Data(utf8).html2AttributedString
+    }
+    var html2String: String {
+        html2AttributedString?.string ?? ""
+    }
+}
