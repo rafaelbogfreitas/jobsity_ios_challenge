b6c7c3ced133100b05e994619deb26f2dfbd5992
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-17 10:25:07 -0300

    Implement Realm object for Serie

--- jobsitychallenge/Models/Database/Serie.swift
+++ jobsitychallenge/Models/Database/Serie.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import RealmSwift
+
+class Serie: Object {
+    @objc dynamic var id = 0
+    @objc dynamic var name = ""
+    let genres = List<String>()
+    @objc dynamic var summary = ""
+}
