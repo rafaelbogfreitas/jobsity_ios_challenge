092141b781608494b016d2592512490f2f0d6d61
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-17 14:11:23 -0300

    Create Entity to map SerieDataResponse and reuse it

---
+++ jobsitychallenge/Models/Entities/SerieDetailsEntity.swift
@@ -0,0 +1,38 @@
+//
+//  SerieDetailsEntity.swift
+//  jobsitychallenge
+//
+//  Created by Rafael Freitas on 17/01/21.
+//
+
+import Foundation
+
+struct SerieDetailsEntity {
+    var id: Int
+    var name: String
+    var genres: [String]
+    var schedule: ShowSchedule? = ShowSchedule(time: "", days: [])
+    var image: PosterTypes? = PosterTypes(medium: "", original: "")
+    var summary: String? = ""
+
+    struct ShowSchedule {
+        var time: String
+        var days: [String]
+    }
+
+    static func mapper(serie: [SerieDataResponse]) -> [SerieDetailsEntity] {
+        serie.map {
+            let schedule = ShowSchedule(time: $0.schedule.time, days: $0.schedule.days)
+
+            return SerieDetailsEntity(id: $0.id, name: $0.name, genres: $0.genres, schedule: schedule, image: $0.image, summary: $0.summary)
+        }
+    }
+
+    static func mapper(searchSeries: [SeriesSearchResponse]) -> [SerieDetailsEntity] {
+        searchSeries.map {
+            let schedule = ShowSchedule(time: $0.show.schedule.time, days: $0.show.schedule.days)
+
+            return SerieDetailsEntity(id: $0.show.id, name: $0.show.name, genres: $0.show.genres, schedule: schedule, image: $0.show.image, summary: $0.show.summary)
+        }
+    }
+}
