8e7246974f52c5b3a8ac23c5d07ee4d9938fc0fa
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-17 10:31:25 -0300

    Update view model with new data structure and method

--- jobsitychallenge/screens/Search/SearchViewModel.swift
+++ jobsitychallenge/screens/Search/SearchViewModel.swift
@@ -9,11 +9,21 @@ import Foundation
 
 class SearchViewModel {
     var series: [SeriesSearchResponse] = []
+    var people: [PeopleSearchResponse] = []
 
     func searchSeries(with string: String, onComplete: @escaping (() -> Void)) {
         ApiManager().request(api: ApiSearch.searchSeries(with: string), type: [SeriesSearchResponse].self) { (series, success) in
             if let series = series, success {
                 self.series = series
+            }
+            onComplete()
+        }
+    }
+
+    func searchPeople(with string: String, onComplete: @escaping (() -> Void)) {
+        ApiManager().request(api: ApiSearch.searchPeople(with: string), type: [PeopleSearchResponse].self) { (people, success) in
+            if let people = people, success {
+                self.people = people
             }
             onComplete()
         }
