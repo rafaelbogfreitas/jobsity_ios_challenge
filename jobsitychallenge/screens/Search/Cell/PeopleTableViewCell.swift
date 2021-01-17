065f971cdf672c4770597cdfa39b0b57fb57dab4
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-17 10:27:22 -0300

    Create cell for displaying people search results

--- jobsitychallenge/screens/Search/Cell/PeopleTableViewCell.swift
+++ jobsitychallenge/screens/Search/Cell/PeopleTableViewCell.swift
@@ -5,4 +5,84 @@
 //  Created by Rafael Freitas on 17/01/21.
 //
 
-import Foundation
+import UIKit
+
+class PeopleTableViewCell: UITableViewCell {
+
+    // MARK: - UI Elements
+
+    lazy var cellLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "\t\t\t"
+        return label
+    }()
+
+    lazy var episodePoster: UIImageView = {
+        let img = UIImageView()
+        img.contentMode = .scaleAspectFit
+
+        return img
+    }()
+
+    // MARK: - Stacks
+
+    lazy var mainStack: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            episodePoster,
+            cellLabel
+        ])
+        stackView.axis = .horizontal
+        stackView.spacing = Constants.stackSpacing
+        return stackView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setConfig()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutIfNeeded() {
+        super.layoutIfNeeded()
+        setConstraints()
+    }
+
+    // MARK: - Config
+
+    private func setConfig() {
+        viewSetup()
+        setConstraints()
+    }
+
+    // MARK: - View SetUp
+
+    private func viewSetup() {
+        self.backgroundColor = UIColor(named: Constants.background)
+        self.selectionStyle = .none
+        self.backgroundColor = UIColor(named: Constants.background)
+        self.addSubview(mainStack)
+    }
+
+    // MARK: - Constraints
+
+    private func setConstraints() {
+        episodePoster.snp.makeConstraints {
+            $0.width.equalTo(frame.width / 2)
+        }
+        mainStack.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(Constants.baseInset)
+        }
+    }
+
+    // MARK: - Layout methods
+
+    func set(serie: PeopleSearchResponse) {
+        cellLabel.text = serie.person.name
+        setImage(image: episodePoster, with: serie.person.image?.medium ?? "")
+    }
+
+}
