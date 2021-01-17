a89463e99989b23ab22b5c875a954e7c58b35d34
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-17 10:29:21 -0300

    Create model for people api response

--- jobsitychallenge/screens/Search/Data/Model/PeopleSearchResponse.swift
+++ jobsitychallenge/screens/Search/Data/Model/PeopleSearchResponse.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct PeopleSearchResponse: Decodable {
+    var score: Float
+    var person: Person
+}
+
+struct Person: Decodable {
+    var id: Int
+    var url: String
+    var name: String
+    var country: Country?
+    var birthday: String?
+    var deathday: String?
+    var gender: String?
+    var image: PosterTypes?
+}
+
+struct Country: Decodable {
+    var name: String
+    var code: String
+    var timezone: String
+}
