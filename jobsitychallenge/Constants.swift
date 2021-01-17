81325ab674069b8192f79afa14361155ffe6d4d6
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-16 21:02:17 -0300

    Create measure and font size constants

--- jobsitychallenge/Constants.swift
+++ jobsitychallenge/Constants.swift
@@ -5,7 +5,7 @@
 //  Created by Rafael Freitas on 15/01/21.
 //
 
-import Foundation
+import UIKit
 
 class Constants {
     // MARK: - Cells identifiers
@@ -16,4 +16,16 @@ class Constants {
     // MARK: - Colors
 
     static var background = "Color_Background"
+
+    // MARK: - Measures
+
+    static var stackSpacing: CGFloat = 20
+    static var baseInset: CGFloat = 20
+
+    // MARK: - Font sizes
+
+    static var smallFont: CGFloat = 8
+    static var mediumFont: CGFloat = 16
+    static var largeFont: CGFloat = 24
+    static var xlargeFont: CGFloat = 32
 }
