6ce41d30307877bd4d9ca8e461dd134287daf432
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-17 10:24:18 -0300

    Create initial Tab controller file

--- jobsitychallenge/Components/TabViewController.swift
+++ jobsitychallenge/Components/TabViewController.swift
@@ -5,4 +5,27 @@
 //  Created by Rafael Freitas on 16/01/21.
 //
 
-import Foundation
+import UIKit
+
+class MainTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.barStyle = .black
+
+        modalPresentationStyle = .fullScreen
+
+        // MARK: - Series List
+        let seriesListController = SeriesListViewController()
+        let seriesListNavController = UINavigationController(rootViewController: seriesListController)
+        seriesListNavController.tabBarItem = UITabBarItem(title: "Series", image: UIImage(systemName: "film"), tag: 0)
+
+        let searchViewController = SearchViewController(isPeopleSearch: true)
+        let searchNavController = UINavigationController(rootViewController: searchViewController)
+        searchNavController.tabBarItem = UITabBarItem(title: "People", image: UIImage(systemName: "person.2"), tag: 1)
+
+        self.viewControllers = [seriesListNavController, searchNavController]
+
+    }
+}
