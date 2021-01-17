6389e5c8e3e4d4f0e2509a74616bf1d682e65a5e
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-01-17 10:33:15 -0300

    Add a fav heart button with action and callback

--- jobsitychallenge/screens/SeriesList/Cell/SeriesListTableViewCell.swift
+++ jobsitychallenge/screens/SeriesList/Cell/SeriesListTableViewCell.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class SeriesListTableViewCell: UITableViewCell {
 
+    // MARK: - Variables
+
+    var didPressFavButton: (() -> Void)?
+
     // MARK: - UI Elements
 
     lazy var cellLabel: UILabel = {
         let label = UILabel()
-        label.isSkeletonable = true
         label.numberOfLines = 0
         label.text = "\t\t\t"
         return label
@@ -21,22 +24,36 @@ class SeriesListTableViewCell: UITableViewCell {
 
     lazy var episodePoster: UIImageView = {
         let img = UIImageView()
-        img.isSkeletonable = true
         img.contentMode = .scaleAspectFit
 
         return img
     }()
 
+    lazy var favButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        button.tintColor = .lightGray
+        button.addTarget(self, action: #selector(favPressed), for: .touchUpInside)
+        return button
+    }()
+
     // MARK: - Stacks
+
+    lazy var contentStack: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            cellLabel,
+            favButton
+        ])
+        return stackView
+    }()
 
     lazy var mainStack: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
             episodePoster,
-            cellLabel
+            contentStack
         ])
         stackView.axis = .horizontal
         stackView.spacing = Constants.stackSpacing
-        stackView.isSkeletonable = true
         return stackView
     }()
 
@@ -65,7 +82,6 @@ class SeriesListTableViewCell: UITableViewCell {
 
     private func viewSetup() {
         self.backgroundColor = UIColor(named: Constants.background)
-        self.contentView.isSkeletonable = true
         self.selectionStyle = .none
         self.backgroundColor = UIColor(named: Constants.background)
         self.addSubview(mainStack)
@@ -87,6 +103,14 @@ class SeriesListTableViewCell: UITableViewCell {
     func set(serie: ShowDataResponse) {
         cellLabel.text = serie.name
         setImage(image: episodePoster, with: serie.image?.medium ?? "")
+    }
+
+    // MARK: - Actions
+
+    @objc func favPressed(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        sender.tintColor = sender.isSelected ? .red : .lightGray
+        didPressFavButton?()
     }
 
 }
